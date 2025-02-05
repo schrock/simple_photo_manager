f4cc05bc99bc128960d2d426dc0245e5da3a39f5
schrock <schrock@users.noreply.github.com>
2025-02-05 14:06:03 -0800

    creation of thumbnails

--- bin/process_photos.sh
+++ bin/process_photos.sh
@@ -17,11 +17,13 @@ cd $(get_script_dir)/..
 
 source ./conf/config
 
-rm -f files.tmp
-rm -f process_log.tmp
+TMP_FILE="files.tmp"
+LOG_FILE="process.log"
 
-find $UNPROCESSED -type f -regex '.*\.\(jpg\|JPG\|jpeg\|JPEG\|png\|PNG\|gif\|GIF\|mp4\|MP4\)$' ! -path '*/.thumbnails/*' > files.tmp
+rm -f "$TMP_FILE"
+rm -f "$LOG_FILE"
 
+find $UNPROCESSED -type f -regex '.*\.\(jpg\|JPG\|jpeg\|JPEG\|png\|PNG\|gif\|GIF\|mp4\|MP4\)$' ! -path '*/.thumbnails/*' > "$TMP_FILE"
 while read -r filepath; do
 	# derive id using exif data
 	id=`exiftool -createdate "$filepath" | cut -d ':' -f 2- | sed 's/[[:space:]]//g;s/://g;s/-//g' | cut -c 1-14`
@@ -31,10 +33,9 @@ while read -r filepath; do
 	# add 00 subseconds by default
 	alt_id="${alt_id}00"
 	if [[ ! "$id" =~ ^[1-9][0-9]{15}$ ]]; then
-		echo "WARNING: Using alt id $alt_id for: $filepath" | tee -a process_log.tmp
+		echo "WARNING: Using alt id $alt_id for: $filepath" | tee -a "$LOG_FILE"
 		id="${alt_id}"
 	fi
-
 	# check id format
 	if [[ "$id" =~ ^[1-9][0-9]{15}$ ]]; then
 		# convert extension to lower case and simplify
@@ -49,10 +50,33 @@ while read -r filepath; do
 			new_filename=${id}.${ext}
 			new_filepath=$PROCESSED/$new_filename
 		done
-		echo "mv \"$filepath\" \"$new_filepath\"" | tee -a process_log.tmp
+		echo "mv \"$filepath\" \"$new_filepath\"" | tee -a "$LOG_FILE"
 		mv "$filepath" "$new_filepath"
 	else
 		# bad or no exif data
-		echo "ERROR: Invalid exif data: $filepath" | tee -a process_log.tmp
+		echo "ERROR: Invalid exif data: $filepath" | tee -a "$LOG_FILE"
 	fi
-done < files.tmp
+done < "$TMP_FILE"
+
+# create thumbnails for jpg, png, gif files
+find $PROCESSED -maxdepth 1 -type f -regex '.*\.\(jpg\|png\|gif\)$' > "$TMP_FILE"
+while read -r filepath; do
+	filename=${filepath##*/}
+	thumbnail="${THUMBNAILS}/${filename}.jpg"
+	if [[ ! -f "$thumbnail" ]]; then
+		echo "Creating thumbnail at $thumbnail" | tee -a "$LOG_FILE"
+		convert -thumbnail x200 -quality 70 "$filepath" "$thumbnail" |& tee -a "$LOG_FILE"
+	fi
+done < "$TMP_FILE"
+
+# create thumbnails for mp4 files
+find $PROCESSED -maxdepth 1 -type f -regex '.*\.\(mp4\)$' > "$TMP_FILE"
+while read -r filepath; do
+	filename=${filepath##*/}
+	thumbnail="${THUMBNAILS}/${filename}.jpg"
+	if [[ ! -f "$thumbnail" ]]; then
+		echo "Creating thumbnail at $thumbnail" | tee -a "$LOG_FILE"
+		ffmpeg -nostdin -hide_banner -loglevel error -y -i "$filepath" -vf "thumbnail=120,scale=w=-1:h=200" -frames:v 1 -q:v 8 "$thumbnail" |& tee -a "$LOG_FILE"
+	fi
+done < "$TMP_FILE"
+
