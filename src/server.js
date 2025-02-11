266147b1b424e6054f67a3fa18a2944298739e1c
schrock <schrock@users.noreply.github.com>
2025-02-11 15:47:06 -0800

    login WIP

--- src/server.js
+++ src/server.js
@@ -4,10 +4,13 @@
 const cluster = require('cluster');
 const os = require('os');
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const http = require('http');
 const fs = require('fs');
+const crypto = require('crypto');
 
-var httpPort = 8081;
+const sessionIdMap = new Map();
 
 if (cluster.isMaster) {
 	var numCPUs = os.cpus().length;
@@ -22,17 +25,67 @@ if (cluster.isMaster) {
 	});
 } else {
 	var app = express();
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(cookieParser());
 	app.use(process.env.WEBBASEDIR, express.static('src/www'));
 	app.use(process.env.WEBBASEDIR + '/node_modules', express.static('node_modules'));
+	app.post(process.env.WEBBASEDIR + '/login', postLogin);
+	app.get(process.env.WEBBASEDIR + '/logout', getLogout);
 	app.get(process.env.WEBBASEDIR + '/data', getData);
 	app.get(process.env.WEBBASEDIR + '/photo', getPhoto);
 	app.get(process.env.WEBBASEDIR + '/trash', getTrashPhoto);
 	app.get(process.env.WEBBASEDIR + '/thumbnail', getThumbnail);
 
 	var server = http.createServer(app);
+	var httpPort = process.env.PORT;
 	server.listen(httpPort, function () {
 		console.log('worker running on port ' + httpPort + '...');
 	});
+}
+
+function postLogin(req, res) {
+	var username = req.body.username;
+	var hash = crypto.createHash('sha512').update(req.body.password).digest('hex');
+	var userEntries = fs.readFileSync('conf/users', {encoding: 'utf8', flag: 'r'}).split("\n");
+	var usernameFound = false;
+	var hashMatches = false;
+	for (var userEntry of userEntries) {
+		if (userEntry.indexOf(":") == -1) {
+			continue;
+		}
+		var userEntryParts = userEntry.split(":", 2);
+		if (userEntryParts[0] != username) {
+			continue;
+		}
+		usernameFound = true;
+		if (userEntryParts[1] == hash) {
+			hashMatches = true;
+		}
+		break;
+	}
+	if (!usernameFound || !hashMatches) {
+		res.status(303);
+		res.setHeader('Location', 'login.html');
+		res.contentType('text/plain');
+		res.send('Invalid credentials.');
+	} else {
+		var sessionId = crypto.randomBytes(16).toString('hex');
+		sessionIdMap.set(sessionId, username);
+		res.setHeader('Set-Cookie', 'sessionId=' + sessionId + '; Path=' + process.env.WEBBASEDIR);
+		res.status(303);
+		res.setHeader('Location', 'index.html');
+		res.contentType('text/plain');
+		res.send('Login successful. Redirecting...');
+	}
+}
+
+function getLogout(req, res) {
+	var sessionId = req.cookies.sessionId;
+	sessionIdMap.delete(sessionId);
+	res.setHeader('Set-Cookie', 'sessionId=' + sessionId + '; Path=' + process.env.WEBBASEDIR + '; expires=Thu, 01 Jan 1970 00:00:00 GMT');
+	res.status(303);
+	res.setHeader('Location', 'login.html');
+	res.send('Logged out. Redirecting to login page...');
 }
 
 function getData(req, res) {
